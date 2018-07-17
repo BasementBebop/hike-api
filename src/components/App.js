825e801614aa2d43853ec1ebc295b156d3057848
Sean Holstrom <basementbebop@Seans-Mac-mini.local>
2018-07-16 21:00:17 -0700

    file structure cleanup and add reakit

---
+++ src/components/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <header>
+          <h1>Hikeboye</h1>
+        </header>
+        <p className="App-intro">
+          Find a sweet spot on Mt Hood
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
