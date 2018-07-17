0f0d8e16de060dd394b3c4c363a47e43dc69a9f2
Sean Holstrom <basementbebop@Seans-Mac-mini.local>
2018-07-16 21:42:44 -0700

    implement basic api fetch in hikeListContainer

--- src/components/HikeListContainer.js
+++ src/components/HikeListContainer.js
@@ -1,10 +1,35 @@
 import React, { Component } from 'react';
 
 class HikeListContainer extends Component {
+  constructor() {
+    super();
+    this.state = {
+      trails: ['trail boye']
+    }
+  }
+
+  componentDidMount() {
+    fetch(`https://www.hikingproject.com/data/get-trails?lat=40.0274&lon=-105.2519&maxDistance=10&key=${process.env.REACT_APP_HIKING_API_KEY}`)
+      .then(res => res.json())
+      .then(data => {
+        this.setState({
+          trails: data.trails
+        });
+      }
+    )
+  }
+
   render() {
     return (
       <div>
         <h1>All them sweet hikes</h1>
+        <ul>
+          {this.state.trails.map(trail => (
+            <li key={trail.id}>
+              {trail.name}
+            </li>
+          ))}
+        </ul>
       </div>
     )
   }
