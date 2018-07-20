fefb4f482403b2ee21cf553cf8f86479e697bd14
Sean Holstrom <basementbebop@Seans-Mac-mini.local>
2018-07-19 19:12:12 -0700

    input values not working...

--- src/components/HikeFilters.js
+++ src/components/HikeFilters.js
@@ -2,18 +2,33 @@ import React, { Component } from 'react';
 import { Input, Label, Button } from 'reakit';
 
 class HikeFilters extends Component {
-  constructor() {
-    super();
-    this.state = { hasResults: false }
+
+  longitudeInput = React.createRef();
+  latitudeInput = React.createRef();
+  rangeInput = React.createRef();
+
+  applyFilters = e => {
+    e.preventDefault();
+    const filters = {
+      latitude: this.latitudeInput.value.value,
+      longitude: this.longitudeInput.value.value,
+      range: this.rangeInput.value.value
+    }
   }
 
   render() {
     return(
-      <form>
-        <Input name="longitude" type="text" placeholder="Longitude"/>
-        <Input name="latitude" type="text" placeholder="Latitude"/>
-        <Input name="range" type="text" placeholder="Range"/>
-        <Button type="submit">Find Hikes</Button>
+      <form onSubmit={this.applyFilters}>
+        <Label>
+          Latitude <Input name="latitude" ref={this.latitudeInput} type="text" defaultValue="45.373690"/>
+        </Label>
+        <Label>
+          Longitude <Input name="longitude" ref={this.longitudeInput} type="text" defaultValue="-121.695936"/>
+        </Label>
+        <Label>
+          Range (max: 200 miles) <Input name="range" ref={this.rangeInput} type="text" defaultValue="50"/>
+        </Label>
+        <Button as="button" type="submit">Find Hikes</Button>
       </form>
     )
   }
